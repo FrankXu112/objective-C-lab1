e34ec83caacc4ae8d52f9f421a8ab7e097363207
Frank Xu <frankxu1992@gmail.com>
2018-09-06 22:02:19 +0800

    Initial Commit

---
+++ Lab1-objective-c/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Lab1-objective-c
+//
+//  Created by Frank Xu on 2018-09-06.
+//  Copyright © 2018 LIUNAN YIN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
